3f7a6c99fd2d2b841d1c6dd04160f66694c1dffe
Butin Artuom <a.butin@andersenlab.com>
2021-10-28 10:31:15 +0300

    adding docker

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,20 @@
 FROM golang:1.17-buster as builder
 
+RUN mkdir /app
+ADD . /app
+
 WORKDIR /app
 
 COPY go.* ./
 RUN go mod download
 
-COPY .. ./
-
-RUN go build -v -o bin/weather/
+RUN go build -o main /app/cmd/
 
 FROM debian:buster-slim
 RUN set -x && apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y \
     ca-certificates && \
     rm -rf /var/lib/apt/lists/*
 
-COPY --from=builder /app/server /app/server
-
-CMD ["/app/server"]
+COPY --from=builder /app/main /app/main
+EXPOSE 8000
+CMD ["/app/main"]
--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main()  {
-	
+import (
+	"fmt"
+	env "github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+
+	})
+	if err := env.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+	port, exists := os.LookupEnv("APP_PORT")
+	if !exists {
+		log.Fatal(".env variable not found")
+	}
+	builder := strings.Builder{}
+	builder.Grow(len(":") + len(port))
+	builder.WriteString(":")
+	builder.WriteString(port)
+	err := http.ListenAndServe(builder.String(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
